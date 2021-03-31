d468f9e26ec54b94c0c06f15ef2f8b8033eaeb1a
Break Yang <breakds@gmail.com>
2021-03-31 14:10:56 -0700

    Add home-manager configuration

--- flake.nix
+++ flake.nix
@@ -7,14 +7,19 @@
     # Use vital-modules, with the same nixpkgs
     vital-modules.url = "github:nixvital/vital-modules?rev=c308db6121905795429a0cd4763c64a544da4703";
     vital-modules.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Use nixos-home, with the same nixpkgs
+    nixos-home.url = "github:breakds/nixos-home?rev=6b28ad77ed02dd51f9d22eafde00ca20917937d6";
+    nixos-home.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, vital-modules, ... }: {
+  outputs = { self, nixpkgs, vital-modules, nixos-home, ... }: {
     nixosConfigurations = {
       welderhelper = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           vital-modules.nixosModules.foundation
+          nixos-home.nixosModules.breakds-home
           ./welderhelper
         ];
       };
