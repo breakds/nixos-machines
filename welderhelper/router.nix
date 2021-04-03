ebde5bedab0efae0039924a3dcf07495277c4bd2
Break Yang <breakds@gmail.com>
2021-04-03 10:40:04 -0700

    Add DHCP configuration for dnsmasq

--- welderhelper/router.nix
+++ welderhelper/router.nix
@@ -50,6 +50,35 @@ in {
   services.dnsmasq = {
     enable = true;
     servers = [ "1.1.1.1" "8.8.8.8" "8.8.4.4" ];
+    # Detailed Explanations
+    # ---------------------
+    # Example: https://thekelleys.org.uk/gitweb/?p=dnsmasq.git;a=blob;f=dnsmasq.conf.example;hb=HEAD#l333
+    #
+    # 1. `dhcp-authoritative` - Quote: Dnsmasq barges in and takes over the lease for any client to
+    #    broadcast on the network, whether it has a record of the lease or not. This avoids long
+    #    timeouts when a machine wakes up on a new network.
+    #
+    # 2. `dhcp-option=option:<option-name>:<option-value>` set options with key value pairs.
+    #
+    # 3. `interface=<interface>` restrict which network Dnsmasq listens to
+    #
+    # 4. `port=0` disables DNS part of Dnsmasq.
+    #
+    # 5. `dhcp-option=option:router:<ip>` overrides the default router/gateway passed down to
+    #    tenant. The default assumes the router is the same machine as the one running dnsmasq.
+    #
+    # 6. `dhcp-option=option:netmask:<ip>` tells the tenant the subnet mask to use.
+    #
+    # 7. `dhcp-range=<ip-start>:<ip-end>:<lease-time>` specifies the IP allocation range and lease
+    #    time for the tenants.
+    extraConfig = ''
+      port=0
+      dhcp-authoritative
+      dhcp-option=option:rouer:10.77.1.1
+      dhcp-option=option:dns-server,1.1.1.1,8.8.8.8,8.8.4.4
+      dhcp-option=option:netmask:255.255.255.0
+      dhcp-range=10.77.1.20,10.77.1.240,24h
+    '';
   };
 
   # Topology for the managed switch:
@@ -78,7 +107,7 @@ in {
     # This is going to be the router's IP to internal devices connects
     # to it.
     ipv4.addresses = [ {
-      address = "10.1.1.1";
+      address = "10.77.1.1";
       prefixLength = 24;  # Subnet Mask = 10.1.1.0/24
     } ];
     useDHCP = false;
