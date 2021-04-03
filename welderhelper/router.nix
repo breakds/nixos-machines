b47dc7669189a8f5cdf7a1bf014dcf5085634234
Break Yang <breakds@gmail.com>
2021-04-03 09:51:35 -0700

    Add dnsmasq: initial commit

--- welderhelper/router.nix
+++ welderhelper/router.nix
@@ -44,6 +44,13 @@ in {
     };
   };
 
+  # Dnsmasq
+  #
+  # Network infrastructure: DNS server and DHCP server
+  services.dnsmasq = {
+    enable = true;
+    servers = [ "1.1.1.1" "8.8.8.8" "8.8.4.4" ];
+  };
 
   # Topology for the managed switch:
   #
