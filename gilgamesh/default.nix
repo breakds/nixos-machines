70aa028218dcedeb50037739b3582824d7ab167c
Break Yang <breakds@gmail.com>
2021-04-30 18:42:58 -0700

    Add my public key to gilgamesh

--- gilgamesh/default.nix
+++ gilgamesh/default.nix
@@ -20,6 +20,12 @@
   config = {
     vital.mainUser = "breakds";
 
+    users.users."breakds" = {
+      openssh.authorizedKeys.keyFiles = [
+        ../data/keys/breakds_samaritan.pub
+      ];
+    };
+
     networking = {
       hostName = "gilgamesh";
       hostId = "7a4bd408";
