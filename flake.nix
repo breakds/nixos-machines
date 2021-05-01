0b737145d62c0ae60cd085a1b575910b8b4286be
Break Yang <breakds@gmail.com>
2021-05-01 08:26:03 -0700

    Update vital-modules

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
 
     # Use vital-modules, with the same nixpkgs
-    vital-modules.url = "github:nixvital/vital-modules?rev=8f1ff5753add79eb4866b94ce996711c127dd999";
+    vital-modules.url = "github:nixvital/vital-modules?rev=a9ca0b3de403e06a0bfde428645ca3bb6f6a4416";
     vital-modules.inputs.nixpkgs.follows = "nixpkgs";
 
     # Use nixos-home, with the same nixpkgs
