936f9180f4c174adcdc3b99b4231827a67c4e1a4
Break Yang <breakds@gmail.com>
2021-05-08 18:08:04 -0700

    Remove ACME

--- hardstone/default.nix
+++ hardstone/default.nix
@@ -36,10 +36,10 @@
     # +----------------+
 
     networking.firewall.allowedTCPPorts = [ 80 443 ];
-    security.acme = {
-      acceptTerms = true;
-      email = "bds@breakds.org";
-    };
+    # security.acme = {
+    #   acceptTerms = true;
+    #   email = "bds@breakds.org";
+    # };
 
     services.nginx = {
       enable = true;
