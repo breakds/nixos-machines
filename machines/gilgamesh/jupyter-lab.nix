2175f59aa57cc924eed1290e5002c98319af010f
Break Yang <breakds@gmail.com>
2021-07-09 13:51:53 -0700

    Add more nginx options for websocket for jupyter

--- machines/gilgamesh/jupyter-lab.nix
+++ machines/gilgamesh/jupyter-lab.nix
@@ -9,7 +9,27 @@
     "lab.breakds.org" = {
       enableACME = true;
       forceSSL = true;
-      locations."/".proxyPass = "http://localhost:7777";
+      locations = {
+        "/".proxyPass = "http://localhost:7777";
+        "~ /api/kernels/" = {
+          proxyPass = "http://localhost:7777";
+          extraConfig = ''
+            proxy_http_version    1.1;
+            proxy_set_header      Upgrade "websocket";
+            proxy_set_header      Connection "Upgrade";
+            proxy_read_timeout    86400;
+          '';
+        };
+        "~ /terminals/" = {
+          proxyPass = "http://localhost:7777";
+          extraConfig = ''
+            proxy_http_version    1.1;
+            proxy_set_header      Upgrade "websocket";
+            proxy_set_header      Connection "Upgrade";
+            proxy_read_timeout    86400;
+          '';
+        };
+      };
     };
   };
 
