baa90773d5dbaf1a245f56bbd727708feb2c6e53
Break Yang <breakds@gmail.com>
2021-08-15 17:03:54 -0700

    Increase power for 3090 ethminer

--- machines/samaritan/default.nix
+++ machines/samaritan/default.nix
@@ -60,10 +60,10 @@
       enable = true;
       recheckInterval = 1000;
       toolkit = "cuda";
-      wallet = "0xcdea2bD3AC8089e9aa02cC6CF5677574f76f0df2.samaritan3060Ti";
+      wallet = "0xcdea2bD3AC8089e9aa02cC6CF5677574f76f0df2.samaritan3090";
       pool = "us2.ethermine.org";
       stratumPort = 4444;
-      maxPower = 240;
+      maxPower = 330;
       registerMail = "";
       rig = "";
     };
