025c2ee88f69d50807e304d66b1d55ff42e2abd0
Break Yang <breakds@gmail.com>
2021-09-02 18:45:55 -0700

    Add iceland.breakds.org

--- machines/richelieu/jupyter-lab.nix
+++ machines/richelieu/jupyter-lab.nix
@@ -31,5 +31,31 @@
         };
       };
     };
+
+    "iceland.breakds.org" = {
+      enableACME = true;
+      forceSSL = true;
+      locations = {
+        "/".proxyPass = "http://localhost:7778";
+        "~ /api/kernels/" = {
+          proxyPass = "http://localhost:7778";
+          extraConfig = ''
+            proxy_http_version    1.1;
+            proxy_set_header      Upgrade "websocket";
+            proxy_set_header      Connection "Upgrade";
+            proxy_read_timeout    86400;
+          '';
+        };
+        "~ /terminals/" = {
+          proxyPass = "http://localhost:7778";
+          extraConfig = ''
+            proxy_http_version    1.1;
+            proxy_set_header      Upgrade "websocket";
+            proxy_set_header      Connection "Upgrade";
+            proxy_read_timeout    86400;
+          '';
+        };
+      };
+    };
   };
 }
