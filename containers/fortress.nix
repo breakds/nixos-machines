fddf3ee249acd447a22d5d3b5135afcff45703c5
Break Yang <breakds@gmail.com>
2021-09-12 09:50:53 -0700

    Allow operator to not to type password for sudo in the container

--- containers/fortress.nix
+++ containers/fortress.nix
@@ -1,9 +1,18 @@
+# Courtesy of
+#
+# 1. IOHK: https://github.com/input-output-hk/ci-ops/blob/2587a1a807ecd19dd33b69557f9c6b33c15b509c/modules/hydra-master-main.nix
+#
+# 2. KJ Orbekk: https://git.orbekk.com/nixos-config.git/tree/config/hydra.nix
+
 {
   imports = [
     ../base/container.nix
   ];
 
-  networking = {
-    hostName = "fortress";
+  config = {
+
+    networking = {
+      hostName = "fortress";
+    };
   };
 }
