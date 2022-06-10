1523b32bd515f6450228fdfeecf806cdbe177af1
Break Yang <breakds@gmail.com>
2022-06-10 13:44:46 -0700

    Upgrade zoom using unstable because currently 22.05 zoom-us will crash upon login with Google

--- base/overlays/default.nix
+++ base/overlays/default.nix
@@ -1,9 +1,9 @@
 final: prev:
 
 let unstablePkgs = import (builtins.fetchTarball {
-      # 2022 Apr 21
-      url = https://github.com/NixOS/nixpkgs/tarball/bfb6f709c032169ea6fa20e2c4c8741a06d5e018;
-      sha256 = "0r8s252lzn9lyg1k45khmw92z325gnswbdm6klq3ch0i66p8nnx6";
+      # June 10 2022
+      url = https://github.com/NixOS/nixpkgs/tarball/0207d018f626f0907f966f232f821ddd8ce054d4;
+      sha256 = "1k4w5sd6kh7lr2gm1jccvv2kmg0n9nssx7hic2n6pgiwc2cppi5r";
     }) {
       config.allowUnfree = true;
       system = prev.system;
@@ -19,4 +19,5 @@ let unstablePkgs = import (builtins.fetchTarball {
 in {
   ethminer = final.callPackage ../../pkgs/temp/ethminer { cudaSupport = true; };
   shuriken = final.callPackage ../../pkgs/shuriken {};
+  zoom-us = unstablePkgs.zoom-us;
 }
