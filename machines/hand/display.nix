a6fc959338ae96dc8621e5a51173d693a470ff30
Break Yang <breakds@gmail.com>
2022-11-26 11:58:54 -0800

    Add detached mode for machines

--- machines/hand/display.nix
+++ machines/hand/display.nix
@@ -5,6 +5,22 @@
     programs.autorandr = {
       enable = true;
       profiles = {
+        # The basic profile when only the laptop screen is used.
+        "detached" = {
+          fingerprint = {
+            "eDP-1" = "00ffffffffffff0009e55f0900000000171d0104a51c137803de50a3544c99260f505400000001010101010101010101010101010101115cd01881e02d50302036001dbe1000001aa749d01881e02d50302036001dbe1000001a000000fe00424f452043510a202020202020000000fe004e4531333546424d2d4e34310a00fb";
+          };
+          config = {
+            # Right
+            "eDP-1" = {
+              enable = true;
+              primary = true;
+              position = "0x0";
+              mode = "2256x1504";
+            };
+          };
+        };
+        
         # Home Profile
         #
         # +-----------------------------------++--------------------+
