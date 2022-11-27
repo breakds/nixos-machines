225559fcd7f12fe1620c0d8296daec88bff70369
Break Yang <breakds@gmail.com>
2022-11-27 00:25:46 -0800

    Malenia to 22.11 with home.stateVersion

--- machines/malenia/default.nix
+++ machines/malenia/default.nix
@@ -103,5 +103,6 @@
     # Before changing this value read the documentation for this option
     # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
     system.stateVersion = "22.05";
+    home-manager.users."breakds".home.stateVersion = "22.05";
   };
 }
