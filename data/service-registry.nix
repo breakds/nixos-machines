a425ecfa23a6ecfc49fec9d73648c78c48e2c50d
Break Yang <breakds@gmail.com>
2023-01-31 09:08:41 -0800

    All web services migration

--- data/service-registry.nix
+++ data/service-registry.nix
@@ -3,4 +3,8 @@ rec {
     domain = "hydra.breakds.org";
     port = 5855;
   };
+  shiori = {
+    domain = "shiori.breakds.org";
+    port = 5931;
+  };
 }
--- machines/octavian/services/web-services.nix
+++ machines/octavian/services/web-services.nix
@@ -1,12 +1,41 @@
 { config, lib, pkgs, ... }:
 
-{
+let shioriInfo = (import ../../../data/service-registry.nix).shiori;
+
+in {
+  # Note that the first time to the web interface, we can use the default user
+  # and password to login:
+  #
+  # username: shiori
+  # password: gopher
+  services.shiori = {
+    enable = true;
+    port = shioriInfo.port;
+  };
+  
   services.nginx = {
     virtualHosts = {
       "www.breakds.org" = {
         enableACME = true;
         forceSSL = true;
         root = pkgs.www-breakds-org;
+      };
+
+      "extorage.breakds.org" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          root = "/var/lib/extorage";
+        };
+      };
+
+      "${shioriInfo.domain}" = {
+        enableACME = true;
+        forceSSL = true;
+
+        locations."/" = {
+          proxyPass = "http://localhost:${toString shioriInfo.port}";
+        };
       };
     };
   };
