0402d37b278dad1fbf7cdedb48654f3465fe42f0
Break Yang <breakds@gmail.com>
2023-02-27 11:17:57 -0800

    Give sudo to lezhao

--- users/lezhao.nix
+++ users/lezhao.nix
@@ -8,7 +8,9 @@
 	    home = "/home/lezhao";
       uid = 2080;
 	    description = "Le Zhao";
-      extraGroups = [];
+      extraGroups = [
+        "wheel"
+      ];
       openssh.authorizedKeys.keyFiles = [
         ../data/keys/lezhao.pub
       ];
