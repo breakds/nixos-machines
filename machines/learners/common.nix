521897d2dc9de487369f701bbe9fea31dd1b0a27
Break Yang <breakds@gmail.com>
2023-04-20 10:49:08 -0700

    Adjust how boot is mounted

--- machines/learners/common.nix
+++ machines/learners/common.nix
@@ -18,10 +18,6 @@
       ];
     };
 
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.efi.efiSysMountPoint = "/boot/efi";
-
     vital.graphical = {
       enable = true;
       remote-desktop.enable = true;
