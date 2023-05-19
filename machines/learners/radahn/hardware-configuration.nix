30f35125e4c6f0bcbc47d948689037414b768a98
Break Yang <breakds@gmail.com>
2023-05-19 15:06:56 -0700

    Update the hardware configuration for radahn

--- machines/learners/radahn/hardware-configuration.nix
+++ machines/learners/radahn/hardware-configuration.nix
@@ -14,23 +14,28 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/dd5fb843-fd44-4e19-8b62-0266dd2444f7";
+    { device = "/dev/disk/by-uuid/2a4da26a-4817-4147-84f9-0ff805b91d7b";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/863A-1385";
+  fileSystems."/boot/efi" =
+    { device = "/dev/disk/by-uuid/6A7D-FE3A";
       fsType = "vfat";
     };
 
-  fileSystems."/home/breakds/dataset" =
-    { device = "/dev/disk/by-label/NIXOS_DATASET";
-      fsType = "ext4";
-    };
-
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/b96e34e1-2755-4565-b8df-01fbe274e16a"; }
+    [ { device = "/dev/disk/by-uuid/feff6d9b-a203-4fee-836f-84bc66aa2f6e"; }
     ];
 
-  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp36s0f0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp36s0f1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp38s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
