d175a2bbea36aaa2bd205ab2a0275e77d936829a
Break Yang <breakds@gmail.com>
2023-06-08 16:11:01 -0700

    Update paperless setup to use the filerun directory

--- machines/octavian/services/paperless.nix
+++ machines/octavian/services/paperless.nix
@@ -7,7 +7,9 @@ in {
     enable = true;
     port = info.port;
     dataDir = "/var/lib/paperless/data";
-    mediaDir = "/var/lib/paperless/media";
+    mediaDir = "/var/lib/filerun/user-files/paperless/media";
+    consumptionDir = "/var/lib/filerun/user-files/paperless/incoming";
+    consumptionDirIsPublic = true;  # Basically "chmod o+w"
     extraConfig = {
       PAPERLESS_ADMIN_USER = "breakds";
     };
@@ -22,5 +24,12 @@ in {
         proxyPass = "http://localhost:${toString info.port}";
       };
     };
+  };
+
+  # Above I have set the comsumption directory and media directory under the
+  # filerun directory. I have added g+w to that directory so that paperless can
+  # read/write those directories as long as it belongs to the "delegator" group.
+  users.users."paperless" = lib.mkIf config.services.paperless.enable {
+    extraGroups = [ "delegator" ];
   };
 }
