a4229b4eea84855ad21f2228000db4bbb10e7de5
Break Yang <breakds@gmail.com>
2023-06-27 17:52:27 -0700

    Disable fkms-3d for now

--- machines/pi/armlet/default.nix
+++ machines/pi/armlet/default.nix
@@ -10,7 +10,7 @@
   # +------------------------------+    
 
   # Enable GPU acceleration
-  hardware.raspberry-pi."4".fkms-3d.enable = true;
+  # hardware.raspberry-pi."4".fkms-3d.enable = true;
 
   hardware.pulseaudio.enable = true;
 
