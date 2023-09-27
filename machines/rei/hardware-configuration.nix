8e98066ebd175aa0a5be54ad66b461af0226da05
Break Yang <breakds@gmail.com>
2023-09-27 11:00:18 -0700

    Update hardware configuration for rei

--- machines/rei/hardware-configuration.nix
+++ machines/rei/hardware-configuration.nix
@@ -8,49 +8,33 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/4fa202de-50a4-44ab-99f8-1d1ee2d6b3a1";
+    { device = "/dev/disk/by-uuid/a20bd791-2727-4a05-9bda-a3bc54f3e1c0";
       fsType = "ext4";
     };
 
-  fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/A0C5-5BE5";
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/4CF2-FA68";
       fsType = "vfat";
     };
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-label/BDS_HOME";
-    fsType = "ext4";
-  };
-
-  # Temporarily removed.
-  # 
-  # fileSystems."/home/breakds/backup" = {
-  #   device = "/dev/disk/by-label/BDS_BACKUP";
-  #   fsType = "ext4";
-  #   options = [ "auto" "nofail" ];
-  # };
-
-  fileSystems."/var/lib/filerun" = {
-    device = "/dev/disk/by-label/NAS_DATA";
-    fsType = "ext4";
-    options = [ "auto" "nofail" ];
-  };
-
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/ba2756a1-d111-4d80-bfb2-3b177dd5cfde"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp9s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
 
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
