a9b7f421b7b5adb6fd27ebd1fa77d6676bde8bd9
Break Yang <breakds@gmail.com>
2023-12-03 10:29:45 -0800

    Update nixos-home

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     vital-modules.inputs.nixpkgs.follows = "nixpkgs";
 
     # Use nixos-home, with the same nixpkgs
-    nixos-home.url = "github:breakds/nixos-home/dev/23.11";
+    nixos-home.url = "github:breakds/nixos-home";
     nixos-home.inputs.nixpkgs.follows = "nixpkgs";
     nixos-home.inputs.home-manager.follows = "home-manager";
 
