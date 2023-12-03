58eae461c24afa4f73cab8e1cd7b31afd2c5b7db
Break Yang <breakds@gmail.com>
2023-12-03 11:06:30 -0800

    disable traintrack octavian

--- machines/octavian/default.nix
+++ machines/octavian/default.nix
@@ -14,7 +14,7 @@
     ./services/hydra.nix
     ./services/media.nix
     ./services/terraria.nix
-    ./services/traintrack.nix
+    # ./services/traintrack.nix
     # TODO(breakds): Fix poetry for 23.05
     # ./services/famass.nix
     ./services/docker-registry.nix
