d02b763d3983c4427614877926928e74c4347c10
Break Yang <breakds@gmail.com>
2024-06-14 10:13:32 -0700

    Add key and home for dustin

--- machines/learners/dustin.nix
+++ machines/learners/dustin.nix
@@ -1,16 +1,54 @@
 { config, lib, pkgs, ... }:
 
 {
-  config.users.extraUsers = {
-    "dustin" = {
-      isNormalUser = true;
-      initialHashedPassword = lib.mkDefault "$5$o2c1SrFVg1xK570h$EO3uklJz1y3SbIPJ5zBUdG6ZYNFKoui3EYa5CX/9j0A";
-	    home = "/home/dustin";
-      uid = 1020;
-	    description = "Dustin Miao";
-      extraGroups = [
-        "dustin"
-      ];
+  config = {
+    users.extraUsers = {
+      "dustin" = {
+        isNormalUser = true;
+        initialHashedPassword = lib.mkDefault "$5$o2c1SrFVg1xK570h$EO3uklJz1y3SbIPJ5zBUdG6ZYNFKoui3EYa5CX/9j0A";
+	      home = "/home/dustin";
+        uid = 1020;
+	      description = "Dustin Miao";
+        extraGroups = [
+          "dustin"
+        ];
+
+        openssh.authorizedKeys.keyFiles = [
+          ../../data/keys/dustin.pub
+        ];
+      };
+    };
+
+    home-manager.users.dustin = {
+      home.stateVersion = "23.11";
+
+      home.file = {
+        ".inputrc".text = ''
+        "\e[A": history-search-backward
+        "\e[B": history-search-forward
+      '';
+      };
+
+      programs.direnv = {
+        enable = true;
+        enableBashIntegration = true;
+        nix-direnv = {
+          enable = true;
+        };
+      };
+
+      programs.git = {
+        enable = true;
+        package = lib.mkDefault pkgs.gitAndTools.gitFull;
+        userName = lib.mkDefault "Dustin Miao";
+        userEmail = lib.mkDefault "dmiao0107@gmail.com";
+
+        extraConfig = {
+          pull.rebase = true;
+          init.defaultBranch = "main";
+          advice.addIgnoredFile = false;
+        };
+      };
     };
   };
 }
