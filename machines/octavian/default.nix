ef0651d4357c9d34dee8eaf3ae3e8956d530ae78
Break Yang <breakds@gmail.com>
2024-06-29 15:02:19 -0700

    Nvidia offload mode

--- machines/octavian/default.nix
+++ machines/octavian/default.nix
@@ -91,6 +91,7 @@
           enable = true;
           intelBusId = "PCI:0:2:0";
           nvidiaBusId = "PCI:1:0:0";
+          offload = true;
         };
       };
       # remote-desktop.enable = false;
