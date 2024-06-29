7e17ce38385240be62d207e44432194038118617
Break Yang <breakds@gmail.com>
2024-06-29 13:59:36 -0700

    More services

--- machines/octavian/default.nix
+++ machines/octavian/default.nix
@@ -12,15 +12,15 @@
     # Other modules
     ./services/web-services.nix
     ./services/monitor.nix
-    # ./services/hydra.nix
-    # ./services/media.nix
+    ./services/hydra.nix
+    ./services/media.nix
     ./services/terraria.nix
     # ./services/traintrack.nix
     # TODO(breakds): Fix poetry for 23.05
     # ./services/famass.nix
     ./services/docker-registry.nix
-    # ./services/paperless.nix
-    # ../../base/tailscale.nix
+    ./services/paperless.nix
+    ../../base/tailscale.nix
   ];
 
   config = {
