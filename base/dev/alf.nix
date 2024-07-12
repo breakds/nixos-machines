a5a4f1889ab935276160fa37eb57d47bbb1967db
Break Yang <breakds@gmail.com>
2024-07-12 16:35:13 -0700

    Add alf to nix registry

--- base/dev/alf.nix
+++ base/dev/alf.nix
@@ -6,5 +6,9 @@
       from = { type = "indirect"; id = "alf-nix-devenv"; };
       to = { type = "path"; path = "/home/breakds/projects/horizon/alf-nix-devenv"; };
     };
+    alf = {
+      from = { type = "indirect"; id = "alf"; };
+      to = { type = "path"; path = "/home/breakds/projects/alf"; };
+    };
   };
 }
