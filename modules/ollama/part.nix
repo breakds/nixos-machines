3de903e6e4748819dd89d08e78a0f70aa43b7146
Break Yang <breakds@gmail.com>
2024-12-01 09:30:05 -0800

    Test local url

--- modules/ollama/part.nix
+++ modules/ollama/part.nix
@@ -38,11 +38,30 @@
           openFirewall = lanExposed;
           environment = {
             # OLLAMA_BASE_URLS is a ";"-separated list. If you have multiple
-            # here, open webui will be able to load-balance them.
+            # here, open webui will be able to load-balance them. Note that open
+            # webui has a backend, which means that the URL here is meant for
+            # the backend, not the frontend (e.g. user's browser).
             OLLAMA_BASE_URLS = "http://127.0.0.1:11434";
             WEBUI_AUTH = "False";
             ANONYMIZED_TELEMETRY = "False";
             DO_NOT_TRACK = "True";
+          };
+        };
+
+        services.avahi = lib.mkIf lanExposed {
+          extraConfig = ''
+            host-name=llm.octavian.local
+            address=10.77.1.131
+          '';
+        };
+
+        services.nginx = lib.mkIf lanExposed {
+          # TODO(breakds): make the host name better.
+          virtualHosts."llm.octavian.local" = {
+            rejectSSL = true;
+            locations."/" = {
+              proxyPass = "http://localhost:11436";
+            };
           };
         };
       };
