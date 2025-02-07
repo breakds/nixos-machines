ea3923653176990bef03ef11f0e8009a61643b63
Break Yang <breakds@gmail.com>
2025-02-07 10:17:26 +0800

    Add n8n test instance to ghostberry

--- machines/ghostberry/default.nix
+++ machines/ghostberry/default.nix
@@ -157,6 +157,13 @@
       builders = [ "kami" ];
     };
 
+    # +--------------------+
+    # | Other Services     |
+    # +--------------------+
+    services.n8n =  {
+      enable = true;
+    };
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
