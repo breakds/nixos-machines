d7d2b3c80f1215d1ebd1dbdd7dda2f4343ec13ee
Break Yang <breakds@gmail.com>
2025-02-24 12:36:13 +0800

    Enable vulkan

--- machines/ghostberry/hardware-configuration.nix
+++ machines/ghostberry/hardware-configuration.nix
@@ -26,6 +26,11 @@
 
   swapDevices = [ ];
 
+  hardware.amdgpu.amdvlk.enable = true;
+  hardware.graphics = {
+    enable = true;
+  };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
