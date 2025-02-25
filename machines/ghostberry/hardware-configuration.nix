3f3beac4a48741906cde89d4ff917a3282251abd
Break Yang <breakds@gmail.com>
2025-02-25 12:36:10 +0800

    Restore ghostberry

--- machines/ghostberry/hardware-configuration.nix
+++ machines/ghostberry/hardware-configuration.nix
@@ -26,9 +26,6 @@
 
   swapDevices = [ ];
 
-  hardware.graphics.enable = true;
-  hardware.amdgpu.amdvlk.enable = true;
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
