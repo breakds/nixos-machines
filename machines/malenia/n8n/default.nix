997e7be5d3084ce0dd8f4a6c08b7ff2d59d49698
Break Yang <breakds@gmail.com>
2025-04-06 11:55:43 -0700

    Update port of n8n

--- machines/malenia/n8n/default.nix
+++ machines/malenia/n8n/default.nix
@@ -4,14 +4,26 @@ let info = (import ../../../data/service-registry.nix).n8n;
 
 in {
   config = {
+
     services.n8n = {
       enable = true;
+      # Note: n8n is already configured to not "phone home" in `services.n8n` via
+      # the environment variable (a.k.a. isolated n8n).
+      #
+      # Some other defaults are
+      #
+      # N8N_USER_FOLDER = "/var/lib/n8n";
+      # HOME = "/var/lib/n8n";
       settings = {
-        port = info.port;
+        port = "${toString info.port}";
         generic.timezone = "America/Los_Angeles";
-        diagnostics.enable = false;
-        versionNotifications.enable = false;
       };
+    };
+
+    # Experiment shows that n8n actually respect this environment variable
+    # rather than `settings.port`.
+    systemd.services.n8n.environment = {
+      N8N_PORT = "${toString info.port}";
     };
   };
 }
