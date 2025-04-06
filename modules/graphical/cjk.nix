b13c28722dc84a61a5e8833a152bd6b682224730
Break Yang <breakds@gmail.com>
2025-04-06 11:50:00 -0700

    Fix warning for deprecation on inputMethod

--- modules/graphical/cjk.nix
+++ modules/graphical/cjk.nix
@@ -4,7 +4,8 @@
   i18n = {
     # Input Method
     inputMethod = {
-      enabled = "fcitx5";
+      enable = true;
+      type = "fcitx5";
       fcitx5.addons = with pkgs; [
         fcitx5-chinese-addons  # This provides pinyin
         fcitx5-gtk
