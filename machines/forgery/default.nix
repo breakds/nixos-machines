ed9c98a1c271e0fbc7be610d228fb0c8f8abbace
Break Yang <breakds@gmail.com>
2025-04-20 18:49:31 -0700

    Add octavian as cache and builder for forgery

--- machines/forgery/default.nix
+++ machines/forgery/default.nix
@@ -94,6 +94,15 @@
       };
     };
 
+    # +--------------------+
+    # | Distributed Build  |
+    # +--------------------+
+
+    vital.distributed-build = {
+      caches = [ "octavian" ];
+      builders = [ "octavian" ];
+    };
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
