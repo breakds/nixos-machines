38498b4839a7f62ef0ca57b568f6860f833c9563
Break Yang <breakds@gmail.com>
2025-04-29 09:43:27 -0700

    Update documentation

--- machines/octavian/services/rustdesk.nix
+++ machines/octavian/services/rustdesk.nix
@@ -1,9 +1,17 @@
 { config, lib, pkgs, ... }:
 
 {
-  # This is strictly for use within the home network. The ID server should be at
-  # `octavian.local:21115`, and the relay servers are not necessary under such
-  # scenario.
+  # This is strictly for use within the home network. The ID server
+  # should be at `octavian.local` or its IP, and the relay servers are
+  # not necessary under such scenario.
+  #
+  # So in the end in the client you need to fill this in the network
+  # settings:
+  #
+  # 1. ID Server: octavian's IP
+  # 2. Relay Server: <blank>
+  # 3. API Server: <blank>
+  # 4. Key: public key, content from /var/lib/rustdesk/id_xxx.pub
   services.rustdesk-server = {
     enable = true;
     signal = {
@@ -11,6 +19,7 @@
       relayHosts = [ "127.0.0.1:21117" ];
     };
     signal.extraArgs = [
+      "--mask" "10.77.1.0/24"
       "-M" "33554432"  # Larger UDP buffer
     ];
     openFirewall = true;
