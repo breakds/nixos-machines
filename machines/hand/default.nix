a7ddc132017480027ff2b64a42ecdbebf11fc5fc
Break Yang <breakds@gmail.com>
2025-05-22 13:47:53 -0700

    Fix locale for hand

--- machines/hand/default.nix
+++ machines/hand/default.nix
@@ -39,7 +39,7 @@
     '';
 
     # Internationalisation
-    i18n.defaultLocale = "en_US.utf8";
+    i18n.defaultLocale = "en_US.UTF-8";
 
     # Enable sound with pipewire.
     hardware.pulseaudio.enable = false;
