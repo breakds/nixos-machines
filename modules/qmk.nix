e2b93851c90d200b749300ce6530f64671e0ffd2
Break Yang <breakds@gmail.com>
2025-09-13 16:42:44 -0700

    Add qmk module

--- modules/qmk.nix
+++ modules/qmk.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
+  # 1. qmk setup qmk/qmk_firmware -H $HOME/projects/qmk/qmk_firmware
+  # 2. qmk cd     # This starts the nix-shell I think
+  # 3. Copy keyboards/geonix40/ from your vendor package into qmk_firmware/keyboards/
   config = {
     services.udev.packages = [ pkgs.qmk-udev-rules ];
     environment.systemPackages = with pkgs; [
