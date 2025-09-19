e5cd9d06bd3dc90d5b944754f260d397d09a005c
Break Yang <breakds@gmail.com>
2025-09-18 22:08:27 -0700

    Add the pilot server

--- machines/octavian/services/psynker.nix
+++ machines/octavian/services/psynker.nix
@@ -23,4 +23,27 @@
       '';
     };
   };
+
+  services.nginx.virtualHosts."pilot.psynk.ai" = {
+    enableACME = true;
+    forceSSL = true;
+
+    locations."~* \\.(js|css|png|jpg|jpeg|gif|ico|svg|woff|woff2)$" = {
+      proxyPass = "http://10.77.1.56:9122";
+      recommendedProxySettings = true;
+      # KEPT: This is for long-term browser caching of static files.
+      extraConfig = ''
+        add_header "Cache-Control" "public, immutable, max-age=31536000";
+      '';
+    };
+
+    locations."/" = {
+      proxyPass = "http://10.77.1.56:9122";
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+      extraConfig = ''
+        add_header "Cache-Control" "no-cache, no-store, must-revalidate";
+      '';
+    };
+  };
 }
