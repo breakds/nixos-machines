d097730df184a4d61a5e794f85f9331913c816e7
Break Yang <breakds@gmail.com>
2025-12-16 17:00:32 -0800

    Support headless screen in sunshine

--- modules/sunshine.nix
+++ modules/sunshine.nix
@@ -3,15 +3,55 @@
 {
   services.sunshine = {
     enable = true;
-    autoStart = true;
+    autoStart = false;
     capSysAdmin = true;
     openFirewall = true;  # Port 47974 - 47990, TCP + UDP
+
+    settings = {
+      output_name = 2;
+    };
   };
 
-  # Enable uinput for mouse/keyboard emulation
-  # Without this, you can see the screen but your Vision Pro input won't work.  
-  # boot.kernelModules = [ "uinput" ];
-  # services.udev.extraRules = ''
-  #   KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
-  # '';
+  systemd.user.services.sunshine = let
+    swaymsg = "${pkgs.sway}/bin/swaymsg";
+
+    jq = "${pkgs.jq}/bin/jq";
+
+    ensureHeadless = pkgs.writeShellScriptBin "sway-ensure-headless" ''
+      set -euo pipefail
+
+      HEADLESS_OUTPUTS=$(
+        ${swaymsg} -t get_outputs -r | ${jq} -r '.[] | select(.name | startswith("HEADLESS-")) | .name'
+      )
+
+      if [ -z "$HEADLESS_OUTPUTS" ]; then
+        ${swaymsg} create_output
+
+        HEADLESS_OUTPUTS=$(
+          ${swaymsg} -t get_outputs -r | ${jq} -r '.[] | select(.name | startswith("HEADLESS-")) | .name'
+        )
+      fi
+
+      for out in $HEADLESS_OUTPUTS; do
+        ${swaymsg} output "$out" mode 3440x1440@60Hz
+      done
+    '';
+
+    cleanupHeadless = pkgs.writeShellScriptBin "sway-cleanup-headless" ''
+      set -euo pipefail
+
+      HEADLESS_OUTPUTS=$(
+        ${swaymsg} -t get_outputs -r | ${jq} -r '.[] | select(.name | startswith("HEADLESS-")) | .name'
+      )
+
+      for out in $HEADLESS_OUTPUTS; do
+        ${swaymsg} output "$out" unplug
+      done
+    '';
+  in {
+    serviceConfig = {
+      ExecStartPre = [ "${ensureHeadless}/bin/sway-ensure-headless" ];
+      ExecStopPost = [ "${cleanupHeadless}/bin/sway-cleanup-headless" ];
+    };
+  };
 }
