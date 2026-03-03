b500435eb74cdce5865da450037a71b0a824efdd
Break Yang <breakds@gmail.com>
2026-03-02 22:50:50 -0800

    Fix the ssh issue

--- base/build-machines-v2.nix
+++ base/build-machines-v2.nix
@@ -52,6 +52,8 @@ in {
           systems = [ "x86_64-linux" "aarch64-linux" ];
           supportedFeatures = [ "kvm" "nixos-test" "big-parallel" "benchmark" ];
           sshUser = if builtins.hasAttr "sshUser" x then x.sshUser else "nixbuilder";
+          sshKey = let keyFile = if builtins.hasAttr "sshKey" x then x.sshKey else "nixbuilder_malenia";
+                   in "/root/.ssh/${keyFile}";
         }) selectedBuilders;
         extraOptions = ''
           builders-use-substitutes = true
