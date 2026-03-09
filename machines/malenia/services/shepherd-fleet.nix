bc4cc101ecc7f7afe70408a567f19e5508dbd8cc
Break Yang <breakds@gmail.com>
2026-03-08 20:31:04 -0700

    Add public key files

--- machines/malenia/services/shepherd-fleet.nix
+++ machines/malenia/services/shepherd-fleet.nix
@@ -4,6 +4,7 @@
   config = {
     services.shepherd-fleet = {
       enable = true;
+      keyFiles = [ ../../../data/keys/breakds_malenia.pub ];
       port = (import ../../../data/service-registry.nix).shepherd.port;
       openFirewall = true;
     };
